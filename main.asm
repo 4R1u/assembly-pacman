cbb072aa34447dbb53a03ed8446d7a82a8c9b649
4R1u <62248461+4R1u@users.noreply.github.com>
2025-04-04 07:24:48 +0500

    feat: drawing loop

--- main.asm
+++ main.asm
@@ -1,304 +1,108 @@
 [org 0x0100]
 	jmp start
 
-font:
-	times 256*16 db 0		; space for font
+mapfilename:
+	db "map.bmp", 0
 
-modeblock:
-	times 256 db 0
-;	.signature db "VBE2"
-;	.table_data: resb 512-4
-; taken from https://forum.osdev.org/viewtopic.php?t=30186
+infobuffer:
+	times 140 db 0
 
-gdt:
-	dd 0x00000000, 0x00000000	; null descriptor
-	dd 0x0000ffff, 0x00cf9a00	; 32bit code descriptor (base(0..15 and 16..23) will be filled later)
-	dd 0x0000ffff, 0x00cf9200	; data
+mappixels:
+	times 500 db 0
 
-gdtreg:
-	dw 0x17				; 16bit limit
-	dd 0				; 32bit base (filled later)
+drawmap:
+	push bp
+	mov bp, sp
+	push ax
+	push bx
+	push cx
+	push dx
+	push ds
+	push es
+	push si
+	push di
 
-helloworld:
-	db "Hello, world! This is a hello world string I need to print, and test line wrapping with. It seems to work fine.", 0
+	mov ax, cs
+	mov ds, ax			; set DS
+	mov ah, 0x3d			; open existing file
+	mov al, 0x00			; read-only
+	mov dx, mapfilename		; set DS:DX
+	int 0x21
+	mov bx, ax			; bx now has file handle
 
-stack: times 256 dd 0			; 1k stack stacktop: start:
-stacktop:
+	mov ah, 0x3f			; read from opened file
+	mov cx, 138			; read 138 bytes
+	mov dx, infobuffer		; into infobuffer
+	int 0x21
+
+; drawing loop
+; in pseudocode
+; func drawmap() {
+;	for (x = no_of_rows; x; --x) {
+;		read file[x] into file_buffer;
+;		for (y = 0; y < no_of_cols; ++y)
+;			es:di = file_bufer[y]
+;		di += no_of_cols;
+;	}
+; }
+
+	mov cx, [infobuffer+0x16]	; number of rows
+	mov di, 0			; this will be used as the "current pixel position"
+rowloop:
+	cmp cx, 0
+	je finisheddrawing
+
+	mov ax, [infobuffer+0x12]	; number of columns
+	mov dx, 0
+	mul cx
+	add ax, [infobuffer+0x0a]	; set pointer to start of row
+	mov dx, ax			; set DS:DX
+	push cx
+
+	mov cx, 0			; CX is MSB
+	mov ax, 0x4200			; set current file position, from origin
+	int 0x21
+
+	mov cx, [infobuffer+0x12]	; number of columns
+	mov ah, 0x3f			; read file
+	mov dx, mappixels		; into "mappixels"
+	int 0x21
+	pop cx
+
+	mov ax, 0xa000
+	mov es, ax			; es points to VRAM
+	mov si, mappixels		; ds:si points to mappixels' start
+
+	push cx
+	mov cx, [infobuffer+0x12]	; repeat for <width> times
+	rep movsb
+	pop cx
+
+	dec cx
+	jmp rowloop
+finisheddrawing:
+	mov ah, 0x3e			; close map file
+	int 0x21
+
+	pop di
+	pop si
+	pop es
+	pop ds
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	pop bp
+	ret
 
 start:
-	mov ax, cs
-	mov es, ax			; point es to cs, just in case
-	mov bp, font			; point di to font block
-	mov ax, 0x1130			; service 11/30, get font info
-	mov bx, 0x0600			; ROM 8x16 font
+	mov ax, 0x13
 	int 0x10
 
-	mov si, bp			; point si to rom font data
-	mov di, font			; point di to font block
-	mov cx, 256*16			; size of 4k font block
-	push ds
-	push es 
-	pop ds				; ds:si to rom font data
-	pop es				; es:di to font block
-	cld				; auto-increment mode
-	rep movsb			; copy font
+	call drawmap
 
-	push cs
-	pop ds				; restore ds to data segment
-
-	mov ax, 0x4f01			; get vesa mode information
-	mov cx, 0x4117			; 1024*768*64k linear framebuffer
-	mov di, modeblock
-	int 0x10
-	mov esi, [modeblock+0x28]	; store address of the first character of the framebuffer
-	; esi now stores the framebuffer mode's equivalent of absolute address 0xB8000
-
-	mov ax, 0x4f02
-	mov bx, 0x4117
-	; 0x0107 is another good option
-	int 0x10			; set vesa mode
-	; the screen should now have a resolution of 1024*768
-
-	mov ax, 0x2401
-	int 0x15			; enable a20
-	; Gate A20 disables address wraparound
-	; this enables it to open the whole memory for us
-
-	xor eax, eax
-	mov ax, cs
-	shl eax, 4
-	mov [gdt+0x08+2], ax		; base(0..15)
-	shr eax, 16
-	mov [gdt+0x08+4], al		; base(16..23)
-	; this should fill in the base of the code descriptor
-	; I think this sets it to the old CS stack register
-
-	xor edx, edx
-	mov dx, cs
-	shl edx, 4
-	add edx, stacktop		; stacktop for protected mode
-	; stacktop points just beyond the 256 dwords of "stack"
-
-	xor ebx, ebx
-	mov bx, cs
-	shl ebx, 4
-	add ebx, font
-	
-	mov ax, cs
-	mov fs, ax
-
-	xor eax, eax
-	mov ax, cs
-	shl eax, 4
-	add eax, gdt
-	mov [gdtreg+2], eax		; set base of gdt
-	lgdt [gdtreg]			; load gdtr
-	; this should load the global descriptor table required for protected mode
-	; its location is cs:gdt
-
-	mov eax, cr0
-	or eax, 1
-
-	cli				; disable interrupts for now
-	mov cr0, eax			; enable protected mode
-	jmp 0x08:pstart			; load cs
-	; protected mode handles interrupts differently
-
-;					;
-;	START OF PROTECTED MODE		;
-;					;
-[bits 32]
-
-;//this is the bitmap font you've loaded
-;unsigned char *font;
-;
-;void drawchar_transparent(unsigned char c, int x, int y, int fgcolor)
-;{
-;	int cx,cy;
-;	int mask[8]={1,2,4,8,16,32,64,128};
-;	unsigned char *glyph=font+(int)c*16;
-;
-;	for(cy=0;cy<16;cy++){
-;		for(cx=0;cx<8;cx++){
-;			if(glyph[cy]&mask[cx]) putpixel(fgcolor,x+cx,y+cy-12);
-;		}
-;	}
-;}
-; from https://wiki.osdev.org/VGA_Fonts#Displaying_a_character
-; this takes 5 arguments, the font area, the character to draw, x, y, and the color
-drawchar:
-	push ebp
-	mov ebp, esp
-	push eax
-	push ebx
-	push ecx
-	push edx
-	push esi
-	push edi
-	
-	mov edx, 0
-	mov eax, 1024 * 2
-	mul dword[ebp+12]		; y
-	mov edi, eax
-	add edi, [fs:modeblock+0x28]
-	mov eax, [ebp+16]		; x
-	shl eax, 1
-	add edi, eax
-
-	mov esi, fs
-	shl esi, 4
-	add esi, font			; 4096 byte font array
-	mov edx, 0
-	mov eax, 16			; each character map is 4 bytes
-	mul dword[ebp+20]		; character
-	add esi, eax
-
-	mov ecx, 0
-dclo:					; drawchar loop (outer)
-	mov bl, 10000000b		; functions as a mask
-	push ecx
-	mov ecx, 0
-
-dcli:					; drawchar loop (inner)
-	test bl, [esi]
-	jz skipwrite
-
-	mov eax, [ebp+8]		; color
-	mov [edi], eax
-
-skipwrite:
-	shr bl, 1
-	add edi, 2
-	inc ecx
-	cmp ecx, 8
-	jl dcli
-
-	add esi, 1
-	add edi, 1024*2-16
-
-	pop ecx
-	inc ecx
-	cmp ecx, 16
-	jl dclo
-
-	pop edi
-	pop esi
-	pop edx
-	pop ecx
-	pop ebx
-	pop eax
-	pop ebp
-	ret 16
-
-pprintstr:
-	push ebp
-	mov ebp, esp
-	push ebx
-
-printloop:
-	mov ebx, [ebp+20]
-	cmp byte[ebx], 0
-	je exitpprintstr
-	movzx ebx, byte[ebx]
-	cmp dword[ebp+16], 1024
-	jne nowrap
-	
-	add dword[ebp+12], 16
-	mov dword[ebp+16], 0
-	
-nowrap:
-	push ebx
-	push dword[ebp+16]
-	push dword[ebp+12]
-	push dword[ebp+8]
-	call drawchar
-	add dword[ebp+20], 1
-	add dword[ebp+16], 8
-	jmp printloop
-
-exitpprintstr:
-	pop ebx
-	pop ebp
-	ret 16
-
-
-pstart:
-	mov ax, 0x10			; load all seg regs to 0x10
-	mov ds, ax			; flat memory model
-	mov es, ax
-;	mov fs, ax
-	mov gs, ax
-	mov ss, ax
-	mov esp, edx		
-
-	mov eax, fs
-	shl eax, 4
-	add eax, helloworld
-	push eax
-	push 1024/2-8
-	push 768/2
-	push 1111100000011111b
-	call pprintstr
-	
-	push 'H'
-	push 1024/2-8
-	push 768/2-16
-	push 0000011111100000b
-	call drawchar
-
-	push 'e'
-	push 1024/2
-	push 768/2-16
-	push 0000011100100000b
-	call drawchar
-
-	push 'l'
-	push 1024/2+8
-	push 768/2-16
-	push 0000011001100000b
-	call drawchar
-
-	push 'l'
-	push 1024/2+16
-	push 768/2-16
-	push 0000010110100000b
-	call drawchar
-
-	push 'o'
-	push 1024/2+24
-	push 768/2-16
-	push 0000010011100000b
-	call drawchar
-
-	push 'W'
-	push 1024/2+40
-	push 768/2-16
-	push 0000010000100000b
-	call drawchar
-
-	push 'o'
-	push 1024/2+48
-	push 768/2-16
-	push 0000001101100000b
-	call drawchar
-
-	push 'r'
-	push 1024/2+56
-	push 768/2-16
-	push 0000001010100000b
-	call drawchar
-
-	push 'l'
-	push 1024/2+64
-	push 768/2-16
-	push 0000000111100000b
-	call drawchar
-
-	push 'd'
-	push 1024/2+72
-	push 768/2-16
-	push 0000000100100000b
-	call drawchar
-
-
-	jmp $
+exit:
+;	mov ax, 3
+;	int 0x10
+	mov ax, 0x4c00
+	int 0x21
