9230be1e1caf7b0d68a01d9710ac0e6b04210649
4R1u <62248461+4R1u@users.noreply.github.com>
2025-04-30 12:58:27 +0500

    feat: make clock faster and pacman slower

--- main.asm
+++ main.asm
@@ -208,7 +208,7 @@ newtimerisr:
 	pop ax
 	
 	call ghostschasepacman
-	cmp byte[time], 2
+	cmp byte[time], 10
 	jnge exitnewtimerisr
 
 	call trymovepacman
@@ -1345,6 +1345,14 @@ displayscoreattheend:
 	ret
 
 start:
+
+	mov al, 0x36
+	out 0x43, al
+	
+	mov ax, 11931
+	out 0x40, al
+	mov al, ah
+	out 0x40, al
 
 	mov eax, 0
 	mov al, 0x00
